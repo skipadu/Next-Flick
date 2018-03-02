81d66d374e18f27d5a989a878b052b2b9a597a4b
Sami Korpela <skipadu@users.noreply.github.com>
2018-03-02 18:12:29 +0200

    Initial commit

---
+++ app/src/main/java/com/pihrit/nextflick/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pihrit.nextflick.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.pihrit.nextflick.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
