7bfaf1ad186729bc7541013b7792485b844f3361
Sami Korpela <skipadu@users.noreply.github.com>
2018-03-15 22:15:12 +0200

    feat: Added URIs to contract

--- app/src/main/java/com/pihrit/nextflick/data/FavoriteMovieContract.java
+++ app/src/main/java/com/pihrit/nextflick/data/FavoriteMovieContract.java
@@ -1,10 +1,19 @@
 package com.pihrit.nextflick.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class FavoriteMovieContract {
 
+    public static final String AUTHORITY = "com.pihrit.nextflick";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    public static final String PATH_FAVORITEMOVIES = "favorites";
+
     public static final class FavoriteMovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVORITEMOVIES).build();
+
         public static final String TABLE_NAME = "favorites";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_MOVIE_ID = "movieId";
